021630c096cba02e462fe7a6882d85ccfe578aeb
Ying Hsuen Lin <yinghsuen@gmail.com>
2019-07-01 11:36:04 -0700

    One more Multiple Choices added

--- app/src/main/java/demc/checkpoint3/QuizOOPMain.java
+++ app/src/main/java/demc/checkpoint3/QuizOOPMain.java
@@ -8,14 +8,18 @@ public class QuizOOPMain
     // this is where the app starts
     public static void main(String[] args)
     {
-        String[] Multi = {"b", "b.", "B", "B.", "9:00"};
-        String[] MChoice = {"a.  8:30", "b.  9:00", "or c.  9:30"};
+        String[] Multi_1 = {"b", "b.", "B", "B.", "9:00"};
+        String[] MChoice_1 = {"a.  8:30", "b.  9:00", "or c.  9:30"};
 
-        Question[] questions = new Question[4];
+        String[] Multi_2 = {"Depends on the work!", "Depends on the work", "depends on the work!", "depends on the work", "c.", "c", "C.", "C"};
+        String[] MChoice_2 = {"a.  11:40~11:50", "b.  11:50~12:00", "or c.  Depends on the work!"};
+
+        Question[] questions = new Question[5];
         questions[0] = new TrueFalseQuestion("Laney is better than BCC?", true);
         questions[1] = new TrueFalseQuestion("Java is an easy to understand language?", false);
         questions[2] = new FillTheBlankQuestion("Which season will the F building be open?", "fall", "Fall 2019", "Maybe never");
-        questions[3] = new MultipleChoiceQuesiton("When does room 274 starts?", MChoice, Multi);
+        questions[3] = new MultipleChoiceQuesiton("When does room 274 starts?", MChoice_1, Multi_1);
+        questions[4] = new MultipleChoiceQuesiton("When does room 274 ends?", MChoice_2, Multi_2);
 
         int index = 0;
         int score = 0;
